e3cf2a772e05f746a468ea851601ee066e023bbf
Kosrat D. Ahmad <kosrat.d.ahmad@gmail.com>
2016-07-27 22:26:45 +0300

    feat: GCE get jokes from jablib then send to the app

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,14 +1,22 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.example.JokeTeller;
 import com.example.android.androidlib.JokeActivity;
+import com.example.kosrat.myapplication.backend.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -43,13 +51,51 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        String joke = new JokeTeller().getJoke();
 
-        Intent intent = new Intent(this, JokeActivity.class);
-        intent.putExtra(JokeActivity.JOKE_KEY, joke);
+        new EndpointsAsyncTask().execute(this);
 
-        startActivity(intent);
     }
 
+    static class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
+        private static MyApi myApiService = null;
+        private Context context;
 
+        @Override
+        protected String doInBackground(Context... params) {
+            if(myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://192.168.1.3:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            context = params[0];
+
+            try {
+                return myApiService.getJokeFromLib().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            Intent intent = new Intent(context, JokeActivity.class);
+            intent.putExtra(JokeActivity.JOKE_KEY, result);
+
+            context.startActivity(intent);
+        }
+    }
 }
--- backend/src/main/java/com/example/kosrat/myapplication/backend/MyEndpoint.java
+++ backend/src/main/java/com/example/kosrat/myapplication/backend/MyEndpoint.java
@@ -6,11 +6,10 @@
 
 package com.example.kosrat.myapplication.backend;
 
+import com.example.JokeTeller;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-
-import javax.inject.Named;
 
 /** An endpoint class we are exposing */
 @Api(
@@ -24,11 +23,12 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    /** A simple endpoint method that takes a joke from javalib */
+    @ApiMethod(name = "getJokeFromLib")
+    public MyBean getJokeFromLib() {
+        JokeTeller joke = new JokeTeller();
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        response.setData(joke.getJoke());
 
         return response;
     }
