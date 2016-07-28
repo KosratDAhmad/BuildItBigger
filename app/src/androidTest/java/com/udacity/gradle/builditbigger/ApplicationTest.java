7c304afa01bd531c82cda0ca9c2f218fefb2732f
Kosrat D. Ahmad <kosrat.d.ahmad@gmail.com>
2016-07-28 21:11:43 +0300

    feat: add functional tests

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -3,11 +3,21 @@ package com.udacity.gradle.builditbigger;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import java.util.concurrent.ExecutionException;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    public void testFetchJokeTask() throws ExecutionException, InterruptedException {
+        MainActivity.EndpointsAsyncTask testJoke = new MainActivity.EndpointsAsyncTask();
+        testJoke.execute(getApplication());
+        String joke = testJoke.get();
+//        Log.i("test", "testFetchJokeTask: " + joke);
+        assertNotNull(joke);
     }
 }
